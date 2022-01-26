3dc04658d3410e1733ef25785d76f99cea05010e
unknown <janoncarlos@gmail.com>
2022-01-26 15:50:16 -0300

    front and back w apollo

--- src/components/containers/Movies.js
+++ src/components/containers/Movies.js
@@ -1,43 +1,54 @@
+import { gql } from "@apollo/client";
+import { graphql } from "graphql";
 import React, { useState } from "react";
 import Card from "./Card";
 
+const allMovies = gql`
+  {
+    movies {
+      name
+      genre
+      year
+    }
+  }
+`;
 const Movies = () => {
-  const [movies, setMovies] = useState([
-    {
-      name: "Terminator",
-      genre: "Action",
-      year: 2000,
-      url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
-    },
-    {
-      name: "Terminator 2",
-      genre: "Action",
-      year: 2002,
-      url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
-    },
-    {
-      name: "Terminator 3",
-      genre: "Action",
-      year: 2003,
-      url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
-    },
-    {
-      name: "Terminator 4",
-      genre: "Action",
-      year: 2004,
-      url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
-    },
-    {
-      name: "Terminator 5",
-      genre: "Action",
-      year: 2005,
-      url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
-    },
-  ]);
+  // const [movies, setMovies] = useState([
+  //   {
+  //     name: "Terminator",
+  //     genre: "Action",
+  //     year: 2000,
+  //     url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
+  //   },
+  //   {
+  //     name: "Terminator 2",
+  //     genre: "Action",
+  //     year: 2002,
+  //     url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
+  //   },
+  //   {
+  //     name: "Terminator 3",
+  //     genre: "Action",
+  //     year: 2003,
+  //     url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
+  //   },
+  //   {
+  //     name: "Terminator 4",
+  //     genre: "Action",
+  //     year: 2004,
+  //     url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
+  //   },
+  //   {
+  //     name: "Terminator 5",
+  //     genre: "Action",
+  //     year: 2005,
+  //     url: "https://www.thedayspring.com.pk/wp-content/uploads/2020/04/movies-to-watch-768x385.jpg",
+  //   },
+  // ]);
 
   return (
     <div className="movies">
-      {movies.map((movie) => {
+      {allMovies.map((movie) => {
         return (
           <Card
             key={movie.name}
@@ -52,4 +63,4 @@ const Movies = () => {
   );
 };
 
-export default Movies;
+export default graphql(allMovies)(Movies);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+
+  cache: new InMemoryCache(),
+});
+
+// function App() {
+//   return (
+//     <div>
+//       <h2>My first Apollo app 🚀</h2>
+//     </div>
+//   );
+// }
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById("root")
 );
